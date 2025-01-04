ecab0c4e936766e690960bdffabe0b0f55e31cf6
allentown <tangxiangpi@gmail.com>
2025-01-04 16:32:51 +0800

    build(scripts): 更新 Popclip 和 Snipdo 扩展图标及名称
    - 将 Pot Translate 改名为 Saladict Translate
    - 更新 Popclip 扩展标识符
    - 修改 Snipdo 扩展名称和标识符
    - 更新扩展描述和图标

--- .scripts/popclip/build.sh
+++ .scripts/popclip/build.sh
@@ -1,7 +1,7 @@
 rm Saladict.popclipextz
-mkdir Pot.popclipext
-cp Config.plist Pot.popclipext
-cp Pot.png Pot.popclipext
-cp Pot.sh Pot.popclipext
-zip -r Saladict.popclipextz Pot.popclipext
-rm -r Pot.popclipext
+mkdir Saladict.popclipextz
+cp Config.plist Saladict.popclipextz
+cp Pot.png Saladict.popclipextz
+cp Pot.sh Saladict.popclipextz
+zip -r Saladict.popclipextz Saladict.popclipextz
+rm -r Saladict.popclipextz
