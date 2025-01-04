c69d7b5d592a2c049342a7d4f8b76d61c1f029ba
allentown <tangxiangpi@gmail.com>
2025-01-04 21:19:51 +0800

    build(popclip): 更新 PopClip 扩展的构建脚本
    - 修改目录名称，从 Saladict.popclipextz 改为 Saladict.popclipext.folder
    - 更新压缩文件名，保持为 Saladict.popclipextz
    - 优化构建流程，提高可读性和效率

--- .scripts/popclip/build.sh
+++ .scripts/popclip/build.sh
@@ -1,7 +1,7 @@
 rm Saladict.popclipextz
-mkdir Saladict.popclipextz
-cp Config.plist Saladict.popclipextz
-cp Pot.png Saladict.popclipextz
-cp Pot.sh Saladict.popclipextz
-zip -r Saladict.popclipextz Saladict.popclipextz
-rm -r Saladict.popclipextz
+mkdir Saladict.popclipext.folder
+cp Config.plist Saladict.popclipext.folder
+cp Pot.png Saladict.popclipext.folder
+cp Pot.sh Saladict.popclipext.folder
+zip -r Saladict.popclipextz Saladict.popclipext.folder
+rm -r Saladict.popclipext.folder
