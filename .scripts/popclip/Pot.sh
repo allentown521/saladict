b8fe537bdf40bf9dad4075aa5bb1a1d0279267bb
allentown <tangxiangpi@gmail.com>
2025-01-23 15:08:09 +0800

    build: 将应用程序名称改为小写
    - 修改了 GitHub Actions 工作流中与应用程序名称相关的路径和文件名，将其从大写改为小写
    - 更新了 Popclip 脚本中应用程序名称的引用，使用小写形式
    - 在 Tauri 配置文件中将 productName 从 "Saladict" 改为 "saladict"
    
    这些更改统一了应用程序名称的大小写，提高了代码和脚本的一致性。

--- .scripts/popclip/Pot.sh
+++ .scripts/popclip/Pot.sh
@@ -3,7 +3,7 @@ curl -Lsd "$POPCLIP_TEXT" "127.0.0.1:60606"
 if [ $? -eq 0 ]; then
     exit 0
 else
-    open -g -a Saladict
+    open -g -a saladict
     sleep 2
     curl -Lsd "$POPCLIP_TEXT" "127.0.0.1:60606/translate"
 fi
