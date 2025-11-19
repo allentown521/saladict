72008a7a912af99b871d37dff03aded41e7a87c8
allentown <tangxiangpi@gmail.com>
2025-11-19 14:34:29 +0800

    fix: use next-ui body background color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -23,8 +23,6 @@ module.exports = {
                 border: 'hsl(var(--border))',
                 input: 'hsl(var(--input))',
                 ring: 'hsl(var(--ring))',
-                background: 'hsl(var(--background))',
-                foreground: 'hsl(var(--foreground))',
                 primary: {
                     DEFAULT: 'hsl(var(--primary))',
                     foreground: 'hsl(var(--primary-foreground))',
@@ -76,7 +74,6 @@ module.exports = {
         },
     },
     plugins: [
-        require('tailwindcss-animate'),
         nextui({
             themes: {
                 dark: {
